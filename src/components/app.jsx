cebb46335e0c94abd389fce28cd25adafcc5db4e
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-07-08 00:31:17 -0400

    Add app component

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import EightThousanderList from '../containers/eight_thousander_list';
+import ActiveEightThousander from '../containers/active_eight_thousander';
+
+const App = () => {
+  return (
+    <div>
+      <EightThousanderList />
+      <ActiveEightThousander />
+    </div>
+  );
+};
+
+export default App;
