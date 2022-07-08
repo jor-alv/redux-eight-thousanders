3b97abd1ddf7f6b508789e2148770dd7acaedb49
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-07-08 00:11:23 -0400

    Add fake database

---
+++ src/data/eight-thousanders.js
@@ -0,0 +1,88 @@
+const mountains = [
+  {
+    name: "Mount Everest",
+    height: 8848,
+    country: "Nepal / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg/1920px-Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg"
+  },
+  {
+    name: "K2",
+    height: 8611,
+    country: "Pakistan / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/cd/K2_Nordseite.jpg"
+  },
+  {
+    name: "Kangchenjunga",
+    height: 8586,
+    country: "Nepal / India",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/57/Kangchenjunga%2C_India.jpg"
+  },
+  {
+    name: "Lhotse",
+    height: 8516,
+    country: "Nepal / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/72/Lhotse-fromChukhungRi.jpg"
+  },
+  {
+    name: "Makalu",
+    height: 8485,
+    country: "Nepal / China",
+    imageUrl: "https://en.wikipedia.org/wiki/Makalu#/media/File:Makalu.jpg"
+  },
+  {
+    name: "Cho Oyu",
+    height: 8188,
+    country: "Nepal / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Cho_Oyu%2C_Nepal.jpg"
+  },
+  {
+    name: "Dhaulagiri I",
+    height: 8167,
+    country: "Nepal",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Dhaulagiri_mountain.jpg/1280px-Dhaulagiri_mountain.jpg"
+  },
+  {
+    name: "Manaslu",
+    height: 8163,
+    country: "Nepal",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Manaslu%2C_from_base_camp_trip.jpg/1280px-Manaslu%2C_from_base_camp_trip.jpg"
+  },
+  {
+    name: "Nanga Parbat",
+    height: 8126,
+    country: "Pakistan",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c1/Northern_Areas_38b_commons.jpg"
+  },
+  {
+    name: "Annapurna I",
+    height: 8091,
+    country: "Nepal",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/South_Face_of_Annapurna_I_%28Main%29.jpg/1280px-South_Face_of_Annapurna_I_%28Main%29.jpg"
+  },
+  {
+    name: "Gasherbrum I / K5",
+    height: 8080,
+    country: "Pakistan / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6f/HiddenPeak.jpg"
+  },
+  {
+    name: "Broad Peak",
+    height: 8051,
+    country: "Pakistan / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5b/7_15_BroadPeak.jpg"
+  },
+  {
+    name: "Gasherbrum II / K4",
+    height: 8035,
+    country: "Pakistan / China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/37/Gasherbrum2.jpg"
+  },
+  {
+    name: "Shishapangma",
+    height: 8027,
+    country: "China",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Shishapangma.jpg"
+  }
+]
+
+export default mountains;
